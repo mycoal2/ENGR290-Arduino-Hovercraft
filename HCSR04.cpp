80232770c23c447618096605ff79c3c79a910309
mycoal2 <92768718+mycoal2@users.noreply.github.com>
2023-06-18 21:55:44 -0400

    hc

---
+++ HCSR04.cpp
@@ -0,0 +1,56 @@
+/*
+  HCSR04 - Library for arduino, for HC-SR04 ultrasonic distance sensor.
+  Created by Martin Sosic, June 11, 2016.
+*/
+
+#include "Arduino.h"
+#include "HCSR04.h"
+
+UltraSonicDistanceSensor::UltraSonicDistanceSensor(
+        byte triggerPin, byte echoPin, unsigned short maxDistanceCm, unsigned long maxTimeoutMicroSec) {
+    this->triggerPin = triggerPin;
+    this->echoPin = echoPin;
+    this->maxDistanceCm = maxDistanceCm;
+    this->maxTimeoutMicroSec = maxTimeoutMicroSec;
+    pinMode(triggerPin, OUTPUT); // 12 --> PB4
+    pinMode(echoPin, INPUT);     // 8 --> PB0
+}
+
+float UltraSonicDistanceSensor::measure_distance_cm() {
+    //Using the approximate formula 19.307°C results in roughly 343m/s which is the commonly used value for air.
+    return measure_distance_cm(19.307);
+}
+
+float UltraSonicDistanceSensor::measure_distance_cm(float temperature) {
+    unsigned long maxDistanceDurationMicroSec;
+
+    // Make sure that trigger pin is LOW.
+    //digitalWrite(triggerPin, LOW); // 12 --> PB4
+    PORTB &= ~(1 << 4);
+    _delay_us(2);
+    // Hold trigger for 10 microseconds, which is signal for sensor to measure distance.
+    //digitalWrite(triggerPin, HIGH); // 12 --> PB4
+    PORTB |= (1 << 4);
+    _delay_us(10);
+    //digitalWrite(triggerPin, LOW);
+    PORTB &= ~(1 << 4);
+
+    float speedOfSoundInCmPerMicroSec = 0.03313 + 0.0000606 * temperature; // Cair ≈ (331.3 + 0.606 ⋅ ϑ) m/s
+
+    // Compute max delay based on max distance with 25% margin in microseconds
+    maxDistanceDurationMicroSec = 2.5 * maxDistanceCm / speedOfSoundInCmPerMicroSec;
+    if (maxTimeoutMicroSec > 0) {
+    	maxDistanceDurationMicroSec = min(maxDistanceDurationMicroSec, maxTimeoutMicroSec);
+    }
+
+    // Measure the length of echo signal, which is equal to the time needed for sound to go there and back.
+    unsigned long durationMicroSec = pulseIn(echoPin, HIGH, maxDistanceDurationMicroSec); // can't measure beyond max distance
+
+    // Serial.print("Pulse Length: "); 
+    // Serial.print(durationMicroSec);
+    // Serial.println();
+
+    float distanceCm = durationMicroSec / 2.0 * speedOfSoundInCmPerMicroSec;
+
+    return distanceCm;
+}
---
+++ hovercraft.ino
@@ -0,0 +1,222 @@
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include <Servo.h>
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "HCSR04.h"
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+#define INTERRUPT_PIN 2
+
+Servo servo0;
+MPU6050 mpu;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q; 
+VectorFloat gravity; 
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+const int MPU = 0x68; // MPU6050 I2C address
+float AccX, AccY, AccZ;
+float GyroX, GyroY, GyroZ;
+float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
+float roll, pitch, yaw;
+float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
+float elapsedTime, currentTime, previousTime;
+int c = 0;
+int j = 0;
+float correct;
+
+
+// US SENSOR SPECIFIC
+int frontTrigPin = 12; // yellow jumper --> pin 12: PB4
+int frontEchoPin = 8;// blue jumper --> pin 8: PB0
+int frontSensorDistance;
+int leftSensorDistance;
+int rightSensorDistance;
+UltraSonicDistanceSensor frontSensor(frontTrigPin, frontEchoPin);
+// UltraSonicDistanceSensor leftSensor(trigPin, echoPin);
+// UltraSonicDistanceSensor rightSensor(trigPin, echoPin);
+
+void setup() {
+// join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    Serial.begin(115200);
+    while (!Serial);
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    devStatus = mpu.dmpInitialize();
+
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        Serial.println(F(")..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+  Wire.begin();                      // Initialize comunication
+  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
+  Wire.write(0x6B);                  // Talk to the register 6B
+  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
+  Wire.endTransmission(true);        //end the transmission
+  calculate_IMU_error();
+  delay(20);
+
+  // COM2A1 --> clear OC2A on compare match
+  // COM2B1 --> clear OC2B on compare match
+  TCCR2A = (1 << COM2A1) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20); // WGM --> WaveForm Generation Mode
+  // set prescaler to 64 (i.e. divide clock speed of timer 1)
+  TCCR2B = (1 << CS22);
+
+    servo0.attach(9);
+}
+
+void loop() {
+
+  if (!dmpReady) return;
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+      ypr[0] = ypr[0] * 180 / M_PI;
+      ypr[1] = ypr[1] * 180 / M_PI;
+      ypr[2] = ypr[2] * 180 / M_PI;
+      if (j <= 300) {
+        correct = ypr[0]; // Yaw starts at random value, so we capture last value after 300 readings
+        j++;
+      } else {
+        ypr[0] = ypr[0] - correct;
+      }
+    Serial.print("Yaw: ");
+    Serial.println(ypr[0]);
+  }
+    frontSensorDistance = frontSensor.measure_distance_cm();
+    // leftSensorDistance  = leftSensor.measure_distance_cm();
+    // rightSensorDistance = rightSensor.measure_distance_cm();
+
+    // Serial.print("Front Distance: ");
+    // Serial.println(frontSensorDistance);
+
+    // Serial.print("Left Distance: ");
+    // Serial.println(leftSensorDistance);
+
+    // Serial.print("Right Distance: ");
+    // Serial.println(rightSensorDistance);
+
+    int servo0Value = map(ypr[0], -90, 90, 0, 180);
+
+    if(frontSensorDistance < 50) {
+      servo0.write(servo0Value);
+    }
+
+
+
+    
+}
+
+void calculate_IMU_error() {
+  // We can call this funtion in the setup section to calculate the accelerometer and gyro data error. From here we will get the error values used in the above equations printed on the Serial Monitor.
+  // Note that we should place the IMU flat in order to get the proper values, so that we then can the correct values
+  // Read accelerometer values 200 times
+  while (c < 200) {
+    Wire.beginTransmission(MPU);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU, 6, true);
+    AccX = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
+    AccY = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
+    AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0 ;  
+    
+    // Sum all readings
+    AccErrorX = AccErrorX + ((atan((AccY) / sqrt(pow((AccX), 2) + pow((AccZ), 2))) * 180 / PI));
+    AccErrorY = AccErrorY + ((atan(-1 * (AccX) / sqrt(pow((AccY), 2) + pow((AccZ), 2))) * 180 / PI));
+    c++;
+  }
+  //Divide the sum by 200 to get the error value
+  AccErrorX = AccErrorX / 200;
+  AccErrorY = AccErrorY / 200;
+  c = 0;
+  // Read gyro values 200 times
+  while (c < 200) {
+    Wire.beginTransmission(MPU);
+    Wire.write(0x43);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU, 6, true);
+    GyroX = Wire.read() << 8 | Wire.read();
+    GyroY = Wire.read() << 8 | Wire.read();
+    GyroZ = Wire.read() << 8 | Wire.read();
+    // Sum all readings
+    GyroErrorX = GyroErrorX + (GyroX / 131.0);
+    GyroErrorY = GyroErrorY + (GyroY / 131.0);
+    GyroErrorZ = GyroErrorZ + (GyroZ / 131.0);
+    c++;
+  }
+  //Divide the sum by 200 to get the error value
+  GyroErrorX = GyroErrorX / 200;
+  GyroErrorY = GyroErrorY / 200;
+  GyroErrorZ = GyroErrorZ / 200;
+  // Print the error values on the Serial Monitor
+  Serial.print("AccErrorX: ");
+  Serial.println(AccErrorX);
+  Serial.print("AccErrorY: ");
+  Serial.println(AccErrorY);
+  Serial.print("GyroErrorX: ");
+  Serial.println(GyroErrorX);
+  Serial.print("GyroErrorY: ");
+  Serial.println(GyroErrorY);
+  Serial.print("GyroErrorZ: ");
+  Serial.println(GyroErrorZ);
+}
